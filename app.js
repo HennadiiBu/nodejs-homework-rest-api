b5b5ad7f29182fb652b219a74cbaa32ab8997436
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-10-09 13:45:31 +0300

    add MongoDB

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-import express, { json } from 'express'
-import logger from 'morgan'
-import cors from 'cors'
+import express, { json } from "express";
+import logger from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import contactsRouter from './routes/api/contacts.js'
+import contactsRouter from "./routes/api/contacts.js";
 
-const app = express()
+dotenv.config();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use('/api/contacts', contactsRouter)
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-export default app
+export default app;
